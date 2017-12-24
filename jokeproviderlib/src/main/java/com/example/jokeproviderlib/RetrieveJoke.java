6355f4c6adc6eab724c50de947e2b4261aa0dbf9
meggamind <aps431@nyu.edu>
2017-12-23 21:55:07 -0800

    initial commit

---
+++ jokeproviderlib/src/main/java/com/example/jokeproviderlib/RetrieveJoke.java
@@ -0,0 +1,12 @@
+package com.example.jokeproviderlib;
+
+/**
+ * Created by aniket on 12/23/17.
+ */
+
+public class RetrieveJoke {
+    public String getJoke() {
+        return "I would love to change the world, but they won’t give me the source code";
+    }
+}
+
